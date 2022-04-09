ad3948f405045ed110153e5d883e2ac6242d6187
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-09 20:02:14 +0530

    Create a simple linked list

--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -1,7 +1,32 @@
 package com.linkedlist;
 
+class Node {
+
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
 public class LinkedListMain {
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to linked list problem");
+		System.out.println("Welcome to LinkedList Datastructure Program");
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(30);
+		Node thirdNode = new Node(70);
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		Node tail = thirdNode;
+
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
 }
